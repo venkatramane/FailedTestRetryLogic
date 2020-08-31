e3741ede9c0550b86fd4f4e8c58d92aac62bbc41
venkatramane <evenkatraman7@gmail.com>
2020-08-31 09:54:15 +0530

    Adding retyr logic for failed test cases

---
+++ src/test/java/usingretryAnalyzer/MyTest.java
@@ -0,0 +1,20 @@
+package usingretryAnalyzer;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class MyTest {
+	
+	@Test
+	public void Test1(){
+		Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void Test2(){
+		Assert.assertEquals(false, true);
+	
+	}
+
+}
